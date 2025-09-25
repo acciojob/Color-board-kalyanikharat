6ef218a826f392714a502041f568c310812102bf
kalyanikharat <132137407+kalyanikharat@users.noreply.github.com>
2025-09-25 10:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,10 +34,11 @@ for (let i = 0; i < TOTAL; i++) {
 
     // After 1 second, revert smoothly (CSS transition handles smoothness)
     sq._tid = setTimeout(() => {
-      sq.style.backgroundColor = '#1b1f26';
-      sq.style.boxShadow = 'none';
-      sq._tid = null;
-    }, 1000);
+  sq.style.backgroundColor = 'rgb(29, 29, 29)';
+  sq.style.boxShadow = 'none';
+  sq._tid = null;
+}, 1000);
+
   });
 
   // Optional: if mouse leaves early, still allow the scheduled fade-back.
