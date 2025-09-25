a39cedb15b59f914ab78d65cf299b97779aca644
kalyanikharat <132137407+kalyanikharat@users.noreply.github.com>
2025-09-25 09:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const container = document.getElementById("Container");
+const squares = 800;
+for (let i = 0; i < squares; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+	
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseout', () => removeColor(square));
+
+  container.appendChild(square);
+}
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+}
+
+function removeColor(element) {
+  setTimeout(() => {
+    element.style.backgroundColor = '#1d1d1d';
+  }, 1000);
+}
+
+function getRandomColor() {
+  const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
